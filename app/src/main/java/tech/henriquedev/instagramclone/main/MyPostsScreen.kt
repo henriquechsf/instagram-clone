ba299e91faef155be9bb7b460d10f5ccc3c3ae2c
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-02-20 18:29:04 -0300

    user image card

--- app/src/main/java/tech/henriquedev/instagramclone/main/MyPostsScreen.kt
+++ app/src/main/java/tech/henriquedev/instagramclone/main/MyPostsScreen.kt
@@ -1,22 +1,28 @@
 package tech.henriquedev.instagramclone.main
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Card
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import tech.henriquedev.instagramclone.IgViewModel
+import tech.henriquedev.instagramclone.R
 
 @Composable
 fun MyPostsScreen(navController: NavController, vm: IgViewModel) {
@@ -91,5 +97,33 @@ fun MyPostsScreen(navController: NavController, vm: IgViewModel) {
 
 @Composable
 fun ProfileImage(imageUrl: String?, onClick: () -> Unit) {
-
+    Box(
+        modifier = Modifier
+            .padding(top = 16.dp)
+            .clickable { onClick.invoke() }
+    ) {
+        UserImageCard(
+            userImage = imageUrl,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(80.dp)
+        )
+        
+        Card(
+            shape = CircleShape,
+            border = BorderStroke(width = 2.dp, color = Color.White),
+            modifier = Modifier
+                .size(32.dp)
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 8.dp, end = 8.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_add),
+                colorFilter = ColorFilter.tint(Color.White),
+                contentDescription = null,
+                modifier = Modifier
+                    .background(Color.Blue)
+            )
+        }
+    }
 }
