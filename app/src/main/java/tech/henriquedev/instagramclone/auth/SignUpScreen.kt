1f7551b3e501cf06bc19441a94db322a87128f16
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-02-20 11:29:33 -0300

    sign up screen UI

--- app/src/main/java/tech/henriquedev/instagramclone/auth/SignUpScreen.kt
+++ app/src/main/java/tech/henriquedev/instagramclone/auth/SignUpScreen.kt
@@ -1,11 +1,92 @@
 package tech.henriquedev.instagramclone.auth
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import tech.henriquedev.instagramclone.IgViewModel
+import tech.henriquedev.instagramclone.R
 
 @Composable
 fun SignUpScreen(navController: NavController, vm: IgViewModel) {
-    Text(text = "SignUpScreen")
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            val usernameState = remember { mutableStateOf(TextFieldValue()) }
+            val emailState = remember { mutableStateOf(TextFieldValue()) }
+            val passState = remember { mutableStateOf(TextFieldValue()) }
+
+            Image(
+                painter = painterResource(id = R.drawable.instagram),
+                contentDescription = "Instagram logo image",
+                modifier = Modifier
+                    .width(84.dp)
+                    .padding(top = 16.dp)
+                    .padding(8.dp)
+            )
+            Text(
+                text = "SignUp",
+                modifier = Modifier.padding(8.dp),
+                fontSize = 30.sp,
+                fontFamily = FontFamily.SansSerif
+            )
+            
+            OutlinedTextField(
+                value = usernameState.value,
+                onValueChange = { usernameState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Username") }
+            )
+            OutlinedTextField(
+                value = emailState.value,
+                onValueChange = { emailState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Email") }
+            )
+            OutlinedTextField(
+                value = passState.value,
+                onValueChange = { passState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Password") }
+            )
+
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text(text = "Sign Up")
+            }
+            
+            Text(
+                text = "Already a user? Go to link ->",
+                color = Color.Blue,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .clickable {  }
+            )
+
+        }
+    }
 }
