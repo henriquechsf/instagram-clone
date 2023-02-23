bf59c267aa4c237047be9f9bf93e7bfeaeb1e943
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-02-23 12:40:48 -0300

    resolve technical debts

--- app/src/main/java/tech/henriquedev/instagramclone/IgViewModel.kt
+++ app/src/main/java/tech/henriquedev/instagramclone/IgViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.toObject
@@ -112,7 +113,36 @@ class IgViewModel @Inject constructor(
     fun uploadProfileImage(uri: Uri) {
         uploadImage(uri) {
             createOrUpdateProfile(imageUrl = it.toString())
+            updatePostImageData(it.toString())
         }
+    }
+
+    private fun updatePostImageData(imageUrl: String) {
+        val currentUid = auth.currentUser?.uid
+        db.collection(POSTS).whereEqualTo("userId", currentUid).get()
+            .addOnSuccessListener {
+                val posts = mutableStateOf<List<PostData>>(arrayListOf())
+                convertPosts(it, posts)
+
+                val refs = arrayListOf<DocumentReference>()
+
+                for (post in posts.value) {
+                   post.postId?.let { id ->
+                       refs.add(db.collection(POSTS).document(id))
+                   }
+                }
+
+                if (refs.isNotEmpty()) {
+                    db.runBatch { batch ->
+                        for (ref in refs) {
+                            batch.update(ref, "userImage", imageUrl)
+                        }
+                    }
+                        .addOnSuccessListener {
+                            refreshPosts()
+                        }
+                }
+            }
     }
 
     private fun createOrUpdateProfile(
@@ -225,7 +255,8 @@ class IgViewModel @Inject constructor(
                 userImage = currentUserImage,
                 postImage = imageUri.toString(),
                 postDescription = description,
-                time = System.currentTimeMillis()
+                time = System.currentTimeMillis(),
+                likes = listOf<String>()
             )
 
             db.collection(POSTS).document(postUuid).set(post)
